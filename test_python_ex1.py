3eab84297b8b7fcdde1c39a6f5498684fecafc93
sabiresd <sabiressaadgenaiot@gmail.com>
2026-05-04 11:55:08 +0100

    Fichier 3

--- test_python_ex1.py
+++ test_python_ex1.py
@@ -1,6 +1,6 @@
 # ===== Exercice 1 =====
-nom = 'Sara'
-age = 20
-ville = 'Casablanca'
-specialite = 'IA'
+nom = input("Entrez votre nom : ")
+age = input("Entrez votre age : ")
+ville = input("Entrez votre ville : ")
+specialite = input("Entrez votre spécialité : ")
 print(f"Bonjour, je m'appelle {nom}, j'ai {age} ans, j'habite à {ville} et je souhaite apprendre {specialite}.")
--- test_python_ex3.py
+++ test_python_ex3.py
@@ -1,6 +1,6 @@
-
-
 # ===== Exercice 3 =====
+somme = 0
 for i in range(1, 11):
     print(i)
-print("Somme =", sum(range(1, 11)))
+    somme = somme + i
+print("Somme =", somme)
